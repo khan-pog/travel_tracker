c96bb3b0bb19a04fa61e9167172f63ba35ddf58d
Khan-pog <Elmo0747>
2021-08-23 20:13:00 +1000

    Initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+"""
+Khan Nathan Kyle Thompson, 23/08/2021,
+s a simple "travel tracker" that allows a user to track places they wish to visit and
+places they have already visited. The program reads and writes a list of places in a text file, GitHub URL)
+"""
